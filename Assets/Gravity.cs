9460a40557533da3fd1fd3887595832c73ff77b4
Yanawat123 <yanawat.buns@bumail.net>
2025-03-13 10:52:19 +0700

    Finished

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -1,13 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
     const float G = 0.006674f;
+    public static List<Gravity> gravityObjectList;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+        gravityObjectList.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList)
+        {
+            if (obj != this)
+
+            {
+                Attract(obj);
+            }
+        }
+
+
     }
 
     void Attract(Gravity other)
